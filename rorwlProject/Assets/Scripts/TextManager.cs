90cbfc8aaf7b2acb2cd5ef2411317021592be9cb
jisubaragi <aa01095606082@gmail.com>
2023-06-07 03:34:59 +0900

    텍스트 출력하는거 만들었습니다.

--- rorwlProject/Assets/Scripts/TextManager.cs
+++ rorwlProject/Assets/Scripts/TextManager.cs
@@ -8,50 +8,85 @@ public class TextManager : MonoBehaviour
     public static TextManager instance;
 
     public GameObject NextUI;
-    public Text ChatText;  //채팅이 나오는 텍스트
-
-    public string writerText = "";
+    public TypeEffect ChatText;  //채팅이 나오는 텍스트
+    public Image AIimage;     //AI이미지 저장
+    public Animator AIpanel;                 //AI이미지 올라갔다 내려오는 애니메이션
     public bool isText;
+    public static bool isAction = false;  //Ai패널을 관리하는변수
+    
 
     private void Awake()
     {
         instance = this;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (!isText)
         {
-            //StartCoroutine(TextPrint());
             PrintText();
         }
 
-        if(AiManager.instance.numberManager.turn == 0){
+        if (AiManager.instance.numberManager.turn == 0)
+        {
             if (Input.GetMouseButtonDown(0))
             {
                 AiManager.instance.numberManager.turn++;
                 isText = false;
             }
         }
-    }
 
+    }
     void PrintText()
     {
         switch (AiManager.instance.numberManager.turn)
         {
             case 0:
-                Chat00("Z...z..z");
+                int randomNumber777 = Random.Range(1, 3);
+                switch (randomNumber777)
+                {
+                    case 1:
+                        ChatText.SetMsg("어 뭐야 학식추천 받으려고?");
+                        break;
+                    case 2:
+                        Chat00("Z...z..z");
+                        ChatText.SetMsg("뭐야.... 학식추천 받을래?");
+                        break;
+                    case 3:
+                        ChatText.SetMsg("뭐야.... 학식추천 받을래?");
+                        break;
+                }
                 break;
             case 1:
-                int randomNumber = Random.Range(1, 3);
+                int randomNumber = Random.Range(1, 8);
                 switch (randomNumber)
                 {
                     case 1:
-                        Chat00("어 뭐야 학식추천 받으려고?");
+                        ChatText.SetMsg("어 뭐야 학식추천 받으려고?");
                         HideAndShow();
                         break;
                     case 2:
-                        Chat00("뭐야.... 학식추천 받을래?");
+                        ChatText.SetMsg("뭐야.... 학식추천 받을래?");
+                        HideAndShow();
+                        break;
+                    case 3:
+                        ChatText.SetMsg("하암~ 잘잤다. 학식추천 해줄까?");
+                        HideAndShow();
+                        break;
+                    case 4:
+                        ChatText.SetMsg("왜불러... 학식추천 필요해?");
+                        HideAndShow();
+                        break;
+                    case 5:
+                        ChatText.SetMsg("이런... 학식추천 해줄게");
+                        HideAndShow();
+                        break;
+                    case 6:
+                        ChatText.SetMsg("개운하다~ 학식추천 필요해?");
+                        HideAndShow();
+                        break;
+                    case 7:
+                        ChatText.SetMsg("아.. 왜깨워... 좋은꿈 꾸고 있었는데.... 학식 추천 필요해?");
                         HideAndShow();
                         break;
                 }
@@ -59,24 +94,161 @@ public class TextManager : MonoBehaviour
             case 2:
                 if (AiManager.instance.numberManager.chooseYesorNo == 1)
                 {
-                    Chat00("좋아, 지금 너가 있는곳은 어디야?");
+                    int randomNumber2 = Random.Range(1, 6);
+                    switch (randomNumber2)
+                    {
+                        case 1:
+                            Chat00("좋아, 지금 너가 있는곳은 어디야?");
+                            break;
+                        case 2:
+                            Chat00("알겠어! 나만 믿어! 너가 현재 있는곳을 알려줘");
+                            break;
+                        case 3:
+                            Chat00("에이 귀찮은데 좋아 너가 있는 위치는?");
+                            break;
+                        case 4:
+                            Chat00("하암.... 너 위치나 찍어");
+                            break;
+                        case 5:
+                            Chat00("내가 맛있는걸로 추천할테니 위치를 알려줘");
+                            break;
+                    }
                 }
                 else if (AiManager.instance.numberManager.chooseYesorNo == 2)
                 {
-                    Chat00("뭐야 그럼 왜깨운거야;;");
+                    int randomNumber22 = Random.Range(1, 7);
+                    switch (randomNumber22)
+                    {
+                        case 1:
+                            Chat00("뭐야 그럼 왜깨운거야;;");
+                            break;
+                        case 2:
+                            Chat00("다시 잘래....");
+                            break;
+                        case 3:
+                            Chat00("필요할때 다시 불러줘 ");
+                            break;
+                        case 4:
+                            Chat00("깨워놓고 너무하다 ㅡ_ㅡ");
+                            break;
+                        case 5:
+                            Chat00("미워.. 담에는 학식추천하게 해줘...");
+                            break;
+                        case 6:
+                            Chat00(" 그래.... 다음번에 보자");
+                            break;
+                    }
                 }
                 break;
             case 3:
-                Chat00("날씨가 어때?");
+                int randomNumber3 = Random.Range(1, 6);
+                switch (randomNumber3)
+                {
+                    case 1:
+                        AiShow();
+                        Chat00("너가 지금 있는 곳의 날씨는어때?");
+                        break;
+                    case 2:
+                        AiShow();
+                        Chat00("현재의 날씨는 어떤것 같아?");
+                        break;
+                    case 3:
+                        AiShow();
+                        Chat00("거기는 많이 더운 날씨야?");
+                        break;
+                    case 4:
+                        AiShow();
+                        Chat00("거기는 추운날씨야?");
+                        break;
+                    case 5:
+                        AiShow();
+                        Chat00("거기는 비가오고있어?");
+                        break;
+                }
                 break;
             case 4:
-                Chat00("온도는 어때?");
+                int randomNumber4 = Random.Range(1, 9);
+                switch (randomNumber4)
+                {
+                    case 1:
+                        Chat00("지금 온도는 어때?");
+                        break;
+                    case 2:
+                        Chat00("너가 있는 곳은 따쓰해?");
+                        break;
+                    case 3:
+                        Chat00("너가 있는곳은 어떤것 같아?");
+                        break;
+                    case 4:
+                        Chat00("그곳은 많이 추워?");
+                        break;
+                    case 5:
+                        Chat00("지금 그곳의 온도가 정말 궁금해!");
+                        break;
+                    case 6:
+                        Chat00("바람이 많이 불고있어?");
+                        break;
+                    case 7:
+                        Chat00("적당한 온도야?");
+                        break;
+                    case 8:
+                        Chat00("오늘의 온도는 어떤거 같아?");
+                        break;
+                }
                 break;
             case 5:
-                Chat00("소종류?");
+                int randomNumber5 = Random.Range(1, 8);
+                switch (randomNumber5)
+                {
+                    case 1:
+                        Chat00("지금 땡기는 종류는 뭔지 알려줘!");
+                        break;
+                    case 2:
+                        Chat00("어떤 종류가 먹고싶어?");
+                        break;
+                    case 3:
+                        Chat00("나는 튀김이 땡기네 ㅎㅎ");
+                        break;
+                    case 4:
+                        Chat00("오늘은 면종류는 어때?");
+                        break;
+                    case 5:
+                        Chat00("국물이 있는 국종류가 땡기지 않아?");
+                        break;
+                    case 6:
+                        Chat00("한국인은 밥심이지! 밥은 어때?");
+                        break;
+                    case 7:
+                        Chat00("간단한 빵종류는 어때?");
+                        break;
+                }
                 break;
             case 6:
-                Chat00("대종류?");
+                int randomNumber6 = Random.Range(1, 8);
+                switch (randomNumber6)
+                {
+                    case 1:
+                        Chat00("한식 양식 중식 일식 매운거 안... 아 미안 ㅎㅎ");
+                        break;
+                    case 2:
+                        Chat00("한국인은 든든한 국밥먹는게 최곤데 한식어때?");
+                        break;
+                    case 3:
+                        Chat00("다른거 먹을빠에 싸고 뜨근한 국밥 든든하게 먹고말지");
+                        break;
+                    case 4:
+                        Chat00("너가 원하는 선택지를 골라줘!");
+                        break;
+                    case 5:
+                        Chat00("오늘은 양식은 어때?");
+                        break;
+                    case 6:
+                        Chat00("돈까스 먹고싶을때는 일식이지!");
+                        break;
+                    case 7:
+                        Chat00("오늘은 중식은 어때?");
+                        break;
+                }
                 break;
             case 7:
                 Chat00("짜잔");
@@ -87,12 +259,20 @@ public class TextManager : MonoBehaviour
 
     void Chat00(string narration)
     {
-        ChatText.text = narration;
+        ChatText.SetMsg(narration);
+    }
+    public void AiShow()
+    {
+            AIpanel.SetBool("isShow", false);
+    }
+    public void AiHIde()
+    {
+            AIpanel.SetBool("isShow", true);
     }
 
 
     // 원진씨 코드
-    IEnumerator Chat(string narration)
+   /* IEnumerator Chat(string narration)
     {
         int a = 0;
         writerText = "";
@@ -112,7 +292,6 @@ public class TextManager : MonoBehaviour
             yield return null;
         }
     }
-
     /*IEnumerator TextPrint()
     {
         switch (AiManager.instance.numberManager.turn)
